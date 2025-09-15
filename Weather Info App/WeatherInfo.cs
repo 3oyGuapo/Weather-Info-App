7c7cdbe3ef12c08edbff1149bb7a127a0401fd74
3oyGuapo <fdafdaq@gmail.com>
2025-09-15 19:14:31 +0100

    Add project files.

---
+++ Weather Info App/WeatherInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Weather_Info_App
+{
+    internal class WeatherInfo
+    {
+        [JsonProperty("Weather")]
+        public WeatherDescription[] Weather { get; set; }
+
+        [JsonProperty("main")]
+        public MainInfo Main { get; set; }
+
+        [JsonProperty("wind")]
+        public WindInfo Wind { get; set; }
+
+        [JsonProperty("name")]
+        public string CityName { get; set; }
+    }
+
+    public class WeatherDescription
+    {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+
+    public class MainInfo
+    {
+        [JsonProperty("temp")]
+        public double Temperature { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
+    }
+
+    public class WindInfo
+    {
+        [JsonProperty("speed")]
+        public double Speed { get; set; }
+    }
+}
